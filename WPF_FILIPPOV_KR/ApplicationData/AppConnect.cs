1847f0696bcb0e67b39d82cbd8a0f09bed31b9a5
superpooperscooper402@gmail.com <superpooperscooper402@gmail.com>
2022-12-09 15:54:01 +1000

    Добавьте файлы проекта.

---
+++ WPF_FILIPPOV_KR/ApplicationData/AppConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_FILIPPOV_KR.ApplicationData
+{
+    class AppConnect
+    {
+        public static KR_FILIPPOVEntities model0db;
+    }
+}
---
+++ WPF_FILIPPOV_KR/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_FILIPPOV_KR.ApplicationData;
+
+namespace WPF_FILIPPOV_KR
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            AppConnect.model0db = new KR_FILIPPOVEntities();
+        }
+
+        private void btnIn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var user0bj = AppConnect.model0db.Client.FirstOrDefault(x => x.Name == txbLogin.Text && x.Familia == psbPassword.Password);
+                if (user0bj == null)
+                {
+                    MessageBox.Show("Ошибка авторизации", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    switch (user0bj.Name)
+                    {
+                        case "1":
+                            MessageBox.Show("Здравствуйте, Клиент" + user0bj.Name + "!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                            break;
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show("Ошибка" + Ex.Message.ToString() + "Критическая ошибка приложения", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+    }
+    }
+}
+
